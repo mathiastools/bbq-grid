77ba086f16c7472521d5ea5d63b8dead8185fb94
Matej Erdos <matej.erdos@emsolutions.sk>
2014-09-13 18:24:26 +0200

    added offset to fixed grid, fixed the fluid grid problems

--- examples/fixed.php
+++ examples/fixed.php
@@ -8,7 +8,7 @@
 	</head>
 	<body>
 		
-		<br><br>
+		<br>
 
 		<div class="container">
 			<div class="row">
@@ -58,41 +58,48 @@
 				<div class="span5">5</div>
 			</div>
 			<div class="row">
-				<div class="span8">
-					d
-				</div>
-				<div class="span4">
-					d
-				</div>
+				<div class="span8">8</div>
+				<div class="span4">4</div>
 			</div>
 			<div class="row">
-				<div class="span9">
-					d
-				</div>
-				<div class="span3">
-					d
-				</div>
+				<div class="span9">9</div>
+				<div class="span3">3</div>
 			</div>
 			<div class="row">
-				<div class="span10">
-					d
-				</div>
-				<div class="span2">
-					d
-				</div>
+				<div class="span10">10</div>
+				<div class="span2">2</div>
 			</div>
 			<div class="row">
-				<div class="span11">
-					d
-				</div>
-				<div class="span1">
-					d
-				</div>
+				<div class="span11">11</div>
+				<div class="span1">1</div>
 			</div>
 			<div class="row">
-				<div class="span12">
-					d
-				</div>
+				<div class="span12">12</div>
+			</div>
+			<div class="row">
+				<div class="span1">1</div>
+				<div class="span1">1</div>
+				<div class="span1">1</div>
+				<div class="span1">1</div>
+				<div class="span1">1</div>
+				<div class="span1">1</div>
+				<div class="span1">1</div>
+				<div class="span1">1</div>
+				<div class="span1">1</div>
+				<div class="span1">1</div>
+				<div class="span1">1</div>
+				<div class="span1">1</div>
+			</div>
+			<div class="row">
+				<div class="span4">4</div>
+				<div class="span3 push5">3 offset 5</div>
+			</div>
+			<div class="row">
+				<div class="span3 push1">3 offset 1</div>
+				<div class="span3 push4">3 offset 4</div>
+			</div>
+			<div class="row">
+				<div class="span6 push6">6 offset 6</div>
 			</div>
 		</div>
 
